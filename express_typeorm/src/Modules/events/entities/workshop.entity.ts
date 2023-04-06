226e4b6fcd76c6202958452fe0cd334cdb9f94d9
Adil Muhammad <100960628+adil-virus-geeks@users.noreply.github.com>
2023-04-06 16:56:34 +0500

    Tasks Completion

--- express_typeorm/src/Modules/events/entities/workshop.entity.ts
+++ express_typeorm/src/Modules/events/entities/workshop.entity.ts
@@ -1,22 +1,23 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Event } from "./event.entity";
 
 @Entity()
 export class Workshop {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'datetime' })
-    start: string;
+  @Column({ type: "datetime" })
+  start: string;
 
-    @Column({ type: 'datetime' })
-    end: string;
+  @Column({ type: "datetime" })
+  end: string;
 
-    @Column({ type: 'integer', default: null })
-    eventId: number;
+  @ManyToOne(() => Event, (event) => event.workshops)
+  event: Event;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column({ type: 'datetime' })
-    createdAt: string;
+  @Column({ type: "datetime" })
+  createdAt: string;
 }
